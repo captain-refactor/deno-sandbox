1a9191f4aa98c178a9e820eed86569f4c113a494
Jan Kremeň <42211280+captain-refactor@users.noreply.github.com>
2021-04-04 22:11:43 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.92.0/http/server.ts";
-const s = serve({ port: 8000 });
-console.log("http://0.0.0.0.:8000/");
+const s = serve({ port: 80 });
+console.log("http://0.0.0.0.:80/");
 for await (const req of s) {
   req.respond({ body: "Hello World\n" });
 }
