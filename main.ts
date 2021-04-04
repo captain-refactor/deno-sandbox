7238842f32ba30286a834b14f31919ce2e00f0a6
Jan Kremeň <42211280+captain-refactor@users.noreply.github.com>
2021-04-04 22:15:58 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-import { serve } from "https://deno.land/std@0.92.0/http/server.ts";
-const s = serve({ port: 80 });
-console.log("http://0.0.0.0.:80/");
-for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
-}
+addEventListener("fetch", (event) => {
+  const response = new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+  event.respondWith(response);
+});
