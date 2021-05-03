0a7ba431f962bdfb94ccdacbcacb2ac08fa18ae3
Jan Kremen <kremenh@gmail.com>
2021-05-03 14:04:18 +0200

    unstable

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-deno install -f --allow-write --allow-read ./cmd/sort-json-keys.ts
+deno install -f --allow-write --allow-read --unstable ./cmd/sort-json-keys.ts
 deno install -f --allow-net --unstable ./cmd/nats-listen.ts
